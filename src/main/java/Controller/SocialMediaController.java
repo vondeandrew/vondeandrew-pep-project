abb45389f12d34dbcc3ecfa79f8c4de9d054c706
Andrew Vonderhaar <vondeandrew@gmail.com>
2025-05-23 12:01:44 +0000

    commit services changes

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -2,8 +2,14 @@ package Controller;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import DAO.AccountDAO;
 import DAO.MessageDAO;
+import Model.Message;
 import Service.AccountService;
 import Service.MessageService;
 
@@ -13,6 +19,8 @@ import Service.MessageService;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    MessageService messServices;
+    AccountService accountServices;
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -21,11 +29,14 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
-        //app.post("/register", this::creatAccount);
-        //app.post("/login", this::login);
-        //app.post("/messages", this::postMessageHandler);
+        app.post("/register", this::creatAccount);
+        app.post("/login", this::login);
+        app.post("/messages", this::postMessageHandler);
         app.get("/messages", this::getAllMessagesHandler);
-        //app.get("/messages/{message_id}", this.getMessageByIDHandler);
+        app.get("/messages/{message_id}", this::getMessageByIDHandler);
+        app.delete("/messages/{message_id}", this::deleteMessageByIDHandler);
+        app.patch("/messages/{message_id}", this::updateMessagebyIDHandler);
+        app.get("/accounts/{account_id}/messages", this::getMessageByID);
 
         return app;
     }
@@ -39,6 +50,46 @@ public class SocialMediaController {
     }
 
     private void getAllMessagesHandler(Context ctx)
+    {
+        List<Message> messages = messServices.getAllMessage();
+        ctx.json(messages);
+    }
+
+    private void creatAccount (Context ctx)
+    {
+
+    }
+
+    private void postMessageHandler (Context ctx)
+    {
+
+    }
+
+    private void getMessageByIDHandler (Context ctx)
+    {
+        String parm = ctx.pathParam("message_id");
+        int idNum = Integer.parseInt(parm);
+        ctx.json(messServices.getMessageByID(idNum));
+    }
+
+    private void deleteMessageByIDHandler (Context ctx)
+    {
+        String parm = ctx.pathParam("message_id");
+        int idNum = Integer.parseInt(parm);
+        ctx.json(messServices.deleteMessageByID(idNum));
+    }
+
+    private void updateMessagebyIDHandler (Context ctx)
+    {
+
+    }
+
+    private void getMessageByID (Context ctx)
+    {
+
+    }
+
+    private void login (Context ctx)
     {
 
     }
